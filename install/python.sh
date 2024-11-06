f31ba8542dc3a20089e45956f1a72bb68710cd8d
Kevin Midboe <kevin.midboe@gmail.com>
2024-11-06 13:52:43 +0100

    install python v8 & v13

--- install/python.sh
+++ install/python.sh
@@ -2,8 +2,8 @@
 brew install python3
 
 # Install specific versions of python
-brew install python@3.12
-brew install python@3.18
+brew install python@3.8
+brew install python@3.13
 
 # Install global python pacakges
 # pip3 install setuptools
